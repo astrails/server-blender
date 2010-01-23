a7236d23c15a5768a3310e1e19066f72fcc2bfbe
Vitaly Kushner <vitaly@astrails.com>
2010-01-23 05:44:38 +0200

    nodes & roles

---
+++ lib/blender/manifest/nodes.rb
@@ -0,0 +1,58 @@
+module Blender
+  module Manifest
+    module Nodes
+
+      # This module encapsulates nodes handeling
+      # Nodes can be declared both on a class level and inside a recipe
+      # When defining a node an 'id' is associated with its hostname
+      # and the node can later be reffered by this id (for example
+      # to get its ip)
+
+      # this holds the map from host ids to hostnames
+      @@internal_hostnames = {nil => Facter.hostname}
+      @@external_hostnames = {nil => Facter.fqdn}
+
+      # returns hostname by id or local host's name if the id is nil
+      # will return the id itself if it can't find the mapping
+      def hostname(id = nil, external = false)
+        (external ? @@external_hostnames : @@internal_hostnames)[id] || id.to_s
+      end
+
+      # return host's IP by its name
+      def addr(name)
+        res = `host #{hostname(name)}`.split("\n").grep(/has address/).first
+        res && res.split.last
+      end
+
+      # same as addr but throws exception if IP can't be found
+      def addr!(name)
+        addr(name) or raise "Can't find host #{name} address"
+      end
+
+      def current_node
+        ENV['NODE'] ||
+        (File.exists?("/etc/node") && File.read("/etc/node").strip) ||
+        hostname
+      end
+
+      def current_node?(id)
+        [id.to_s, @@internal_hostnames[id], @@external_hostnames[id]].compact.include?(current_node)
+      end
+
+      def node(id, internal_name = nil, external_name = internal_name)
+        return if false == internal_name # can be used to temporary 'disable' host's definition
+                                         # like:
+                                         # host :app2, false do .. end
+        @@internal_hostnames[id] = internal_name if internal_name
+        @@external_hostnames[id] = external_name if external_name
+
+        if block_given? && current_node?(id)
+          puts "NODE: #{id} / #{current_node}"
+          @node = id
+          yield
+        end
+      end
+
+    end
+  end
+end
---
+++ lib/blender/manifest/roles.rb
@@ -0,0 +1,34 @@
+module Blender
+  module Manifest
+    module Roles
+
+      # A very simple mechanism to define roles
+      #
+      # Roles to accept can be defined from environment, /etc/roles file
+      # or can be explicitly defined like 'roles xxx, yyy'
+      # usually the 'roles a,b,c' call will come inside of
+      # a `node` block and so will be conditionally executed depending on the
+      # running host
+
+      @@current_roles = if ENV['ROLES']
+        ENV['ROLES'].split
+      elsif File.exists?("/etc/roles")
+        File.read("/etc/roles").strip.split
+      else
+        []
+      end
+
+      def roles *roles
+        @@current_roles += roles.map {|r| r.to_s}
+      end
+
+      def role r
+        if block_given? && @@current_roles.include?(r.to_s)
+          puts "ROLE: #{r}"
+          yield
+        end
+      end
+
+    end
+  end
+end
