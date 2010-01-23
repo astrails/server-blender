7910d2eb7e9e7cda2ddca8c86c085971da5c48b5
Vitaly Kushner <vitaly@astrails.com>
2010-01-23 05:00:54 +0200

    support for upstream gems

--- lib/blender/cli/init.rb
+++ lib/blender/cli/init.rb
@@ -1,8 +1,14 @@
-options = {}
+options = {
+  :system_gems => 'y'
+}
 OptionParser.new do |opts|
   opts.banner = "Usage: blender init [OPTIONS] HOST"
   opts.separator ""
   opts.separator "Common options:"
+
+  opts.on("-u", "--upstream-gems", "don't use the sytem gems, download and install upstream version instead") do
+    options[:system_gems] = 'n'
+  end
 
   opts.on("-h", "--help", "Show this message") do
     puts opts
@@ -14,4 +20,4 @@ end.parse!
 abort("please provide a hostname") unless host = ARGV.shift
 
 system("scp", path("files/bootstrap.sh"), "#{host}:/tmp/bootstrap.sh") &&
-system("ssh", host, "/bin/bash /tmp/bootstrap.sh")
+system("ssh", host, "USE_SYSTEM_GEMS=#{options[:system_gems]} /bin/bash /tmp/bootstrap.sh")
